6d9d32e358026898fe36062e6de1476ac651abcc
Yara Prins <yaraprins@live.nl>
2022-02-21 21:48:04 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -16,53 +16,133 @@ const svgDice_overMij = document.querySelector("body header nav ul li:nth-of-typ
 const svgDice_mijnWerk = document.querySelector("body header nav ul li:nth-of-type(3) a svg:nth-of-type(2)");
 const svgDice_contact = document.querySelector("body header nav ul li:nth-of-type(4) a svg:nth-of-type(2)");
 
-// Home
 
-navHome.addEventListener("mouseover", () => {
-    svg_home.style.opacity = "1";
-    svgDice_home.style.opacity = "1";
-});
+const mediaQuery = window.matchMedia("(max-width: 800px)")
 
-navHome.addEventListener("mouseleave", () => {
-    svg_home.style.opacity = "0";
-    svgDice_home.style.opacity = "0";
-});
+function desktopNav(e) {
 
-// Over Mij
+    if (e.matches) {
+        svg_home.style.opacity = "1";
+        svgDice_home.style.opacity = "1";
+        svg_overMij.style.opacity = "1";
+        svgDice_overMij.style.opacity = "1";
+        svg_mijnWerk.style.opacity = "1";
+        svgDice_mijnWerk.style.opacity = "1";
+        svg_contact.style.opacity = "1";
+        svgDice_contact.style.opacity = "1";
 
-navOverMij.addEventListener("mouseover", () => {
-    svg_overMij.style.opacity = "1";
-    svgDice_overMij.style.opacity = "1";
-});
 
-navOverMij.addEventListener("mouseleave", () => {
-    svg_overMij.style.opacity = "0";
-    svgDice_overMij.style.opacity = "0";
-});
+        navHome.removeEventListener("mouseover", mouseOverHome);
+    
+        navHome.removeEventListener("mouseleave", mouseLeaveHome);
+    
+        // Over Mij
+        navOverMij.removeEventListener("mouseover", mouseOverOverMij);
+    
+        navOverMij.removeEventListener("mouseleave",  mouseLeaveOverMij);
+    
+        // Mijn Werk
+        navMijnWerk.removeEventListener("mouseover", mouseOverMijnWerk);
+    
+        navMijnWerk.removeEventListener("mouseleave", mouseLeaveMijnWerk);
+    
+        // Contact
+        navContact.removeEventListener("mouseover", mouseOverContact);
+    
+        navContact.removeEventListener("mouseleave", mouseLeaveContact);
 
-// Mijn Werk
 
-navMijnWerk.addEventListener("mouseover", () => {
-    svg_mijnWerk.style.opacity = "1";
-    svgDice_mijnWerk.style.opacity = "1";
-});
+    
+    } 
+    
+    else if (!e.matches)  {
+         // Home
 
-navMijnWerk.addEventListener("mouseleave", () => {
-    svg_mijnWerk.style.opacity = "0";
-    svgDice_mijnWerk.style.opacity = "0";
-});
+         svg_home.style.opacity = "0";
+         svgDice_home.style.opacity = "0";
+         svg_overMij.style.opacity = "0";
+         svgDice_overMij.style.opacity = "0";
+         svg_mijnWerk.style.opacity = "0";
+         svgDice_mijnWerk.style.opacity = "0";
+         svg_contact.style.opacity = "0";
+         svgDice_contact.style.opacity = "0";
+ 
+ 
+         function mouseOverHome () {
+             svg_home.style.opacity = "1";
+             svgDice_home.style.opacity = "1";
+         };
+         function mouseLeaveHome () {
+             svg_home.style.opacity = "0";
+             svgDice_home.style.opacity = "0";
+         };
+ 
+         function mouseOverOverMij () {
+             svg_overMij.style.opacity = "1";
+             svgDice_overMij.style.opacity = "1";
+         };
+         function mouseLeaveOverMij () {
+             svg_overMij.style.opacity = "0";
+             svgDice_overMij.style.opacity = "0";
+         };
+ 
+         function mouseOverMijnWerk () {
+             svg_mijnWerk.style.opacity = "1";
+             svgDice_mijnWerk.style.opacity = "1";
+         };
+         function mouseLeaveMijnWerk () {
+             svg_mijnWerk.style.opacity = "0";
+             svgDice_mijnWerk.style.opacity = "0";
+         };
+ 
+         function mouseOverContact () {
+             svg_contact.style.opacity = "1";
+             svgDice_contact.style.opacity = "1";
+         };
+         function mouseLeaveContact () {
+             svg_contact.style.opacity = "0";
+             svgDice_contact.style.opacity = "0";
+         };
+ 
+         navHome.addEventListener("mouseover", mouseOverHome);
+     
+         navHome.addEventListener("mouseleave", mouseLeaveHome);
+     
+         // Over Mij
+         navOverMij.addEventListener("mouseover", mouseOverOverMij);
+     
+         navOverMij.addEventListener("mouseleave",  mouseLeaveOverMij);
+     
+         // Mijn Werk
+         navMijnWerk.addEventListener("mouseover", mouseOverMijnWerk);
+     
+         navMijnWerk.addEventListener("mouseleave", mouseLeaveMijnWerk);
+     
+         // Contact
+         navContact.addEventListener("mouseover", mouseOverContact);
+     
+         navContact.addEventListener("mouseleave", mouseLeaveContact);
+    }
+}
 
-// Contact
+desktopNav(mediaQuery)
 
-navContact.addEventListener("mouseover", () => {
-    svg_contact.style.opacity = "1";
-    svgDice_contact.style.opacity = "1";
-});
+mediaQuery.addEventListener("change", desktopNav);
 
-navContact.addEventListener("mouseleave", () => {
-    svg_contact.style.opacity = "0";
-    svgDice_contact.style.opacity = "0";
-});
+
+
+
+/* 
+==================================
+==================================
+==================================
+==================================
+
+OMZETTEN VAN windowPosition = window.scrollY 
+NAAR Intersection Observer
+
+==================================
+*/
 
 
 window.addEventListener("scroll", () => {
