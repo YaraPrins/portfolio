07dde89ae00f9848aab7ec74ec1711c1dd7c2239
Yara Prins <yaraprins@live.nl>
2022-02-23 12:44:04 +0100

    finished intersection observer, responsive done!

--- script.js
+++ script.js
@@ -1,24 +1,34 @@
 const nav = document.querySelector("body header nav");
 const navToggle = document.querySelector(".mobNav-trigger");
 
+const navList = document.querySelectorAll(".navMob li");
+
 
 const sections = document.querySelectorAll ("section");
 const options = {
-    threshold: "0.6",
-
+    threshold: "0.1",
 };
 
-const observer = new IntersectionObserver((entries) => {
+const observer = new IntersectionObserver(entries => {
     entries.forEach((e) => {
         if (e.isIntersecting) {
+
+            nav.classList.add("active");
             console.log(e.target.id);
 
+            navList.forEach(link => {
 
+                link.classList.remove("active");
+
+                if (e.target.id === link.dataset.nav) {
+                    link.classList.add("active");
+                }
+            })
         }
     });
 }, options);
 
-sections.forEach((section) => {
+sections.forEach(section => {
     observer.observe(section);
 });
 
